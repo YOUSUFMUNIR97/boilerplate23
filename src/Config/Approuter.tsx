d5273d993ec121e3f8c1e8a42e2cea6556f7c61c
Muhammad Yousuf <yousuf.munir97@gmail.com>
2023-09-17 20:40:05 +0500

    projectsteup npm installed

---
+++ src/Config/Approuter.tsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import Dashboard from '../Screens/Dashboard'
+import Login from '../Screens/Login'
+import Signup from '../Screens/Signup'
+
+
+const Approuter = () => {
+  return (
+      <>
+      <Router>
+        <Routes>
+            <Route  path='/' element={<Dashboard/>}/>
+            <Route  path='Login' element={<Login/>}/>
+            <Route  path='Signup' element={<Signup/>}/>
+        </Routes>
+
+      </Router>
+      
+      </>
+    
+    )
+}
+
+export default Approuter
